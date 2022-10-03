b3c5bf32298a6dc1f78ba760019f92a4b324f976
Justin C <justin1998m1@gmail.com>
2022-10-02 20:44:52 -0400

    added some comments

--- js-gi-3/hard/pii.js
+++ js-gi-3/hard/pii.js
@@ -1,15 +1,14 @@
-//make object pii
-  //contains
-    //public name
-    //private ssn
 function generatePII(){
+  //pii cannot be accessed outside function due to let
   let pii = {
     name : "Joe Schmoe",
     ssn: '049-49-9321'
   }
+  //returns the name to the caller
   function getName(){
     return pii.name;
   }
+  //calles get name as a default behavior to utilize the closur
   return getName();
 }
 
